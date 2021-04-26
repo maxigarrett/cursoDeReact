7c77b637e004a6b18ab71ac9630cf9a50daecb37
maxigarrett <maxyg22@outlook.es>
2021-04-26 16:56:59 -0300

    estado modificacion de variables en clases

---
+++ 4-estado/src/componentes/Estado.js
@@ -0,0 +1,56 @@
+/*El estado no es mas que al conjunto de variables que va a tener nuestros componentes, es decir, el estado
+no es mas que las variables que intervienen en la modificacion de nuestro componente como un <p>,<h1>,etc
+y que valores tiene en el momento que se esta ejecutando.
+el estado tiene tres caracteristicas importantes:
+    1-es inmutable: es decir cuando se modifica el estado se genera una copia y luego se le agrega lo nuevo
+    a esa copia y se genera un nuevo estado.
+    2-No se puede modificar directamente:
+    3-es asincrono
+    
+para hacer cambios hay que hacer uso del metodo setState()*/
+import React,{Component} from 'react';
+
+//se puede pasar estados de padres a hijos osea a este  componente y le pasaremos el contador ya que todavia
+//no vimos hooks y no podemos pasarl estado a las funciones entonces le pasamos el estado como propiedad
+//a este componente
+const EstadoAHijo=(props)=>{
+    return(
+        <div>
+            <h3>{props.contadorHijo}</h3>
+        </div>
+    );
+}
+//mas adelante con las hucks podremos usar funciones con estados y descartar las clases
+class Estado extends Component{
+    //si newcesitamos que este componente tenga un estado lo hacemos mediante el constructor super()
+    constructor(props){
+        super(props)//llamamos el constructo del meto que hereda Estado que es Component
+        this.state={
+            contador:0
+        }
+
+        //2-no se puede modificar directamente
+        setInterval(()=>{
+            //no se puede modificar directamente la propiedad contador el objeto state hay que hacerlo mediante
+            //el metodo setState()
+            // this.state.contador+1
+            this.setState({
+              contador:this.state.contador+1
+            })
+        },1000)
+    }
+    render(){
+        return(
+            <div>
+                <h2>El state</h2>
+                {/*al modificar un elemento como este P que cada tanto se modificara su contenido por el 
+                setInterval por detras react se encarga de renderizar solo que se modifica y no todo de nuevo*/}
+                <p>{this.state.contador}</p>
+                {/*le pasamos el estado contador a un componente hijo llamado contadorHijo*/}
+                <EstadoAHijo contadorHijo={this.state.contador}></EstadoAHijo>
+            </div>
+        );
+    }
+}
+
+export default Estado
