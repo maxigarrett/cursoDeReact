4a0065173b75f2a3766b8351728ad5ab11d6debb
maxigarrett <62960428+maxigarrett@users.noreply.github.com>
2021-06-24 10:58:47 -0300

    Update SelectAnidados.js

--- 22-select-anidados-api-paises/src/component/SelectAnidados.js
+++ 22-select-anidados-api-paises/src/component/SelectAnidados.js
@@ -5,16 +5,27 @@ export const SelectAnidados=()=>{
     const [state,setState]=useState(null)
     const [municipios,setMunicipios]=useState('')
     const [colonias,setColonias]=useState('')
+    const Token='d81a7ac7-976d-4e1e-b7d3-b1979d791b6c'
     return(
         <>
             <h2>Select Anidados</h2>
-            <SelectList title='estados' url={``} handleChange={(e)=>setState(e.target.value)}/>
+            <SelectList 
+                title='estado'
+                url={`https://api.copomex.com/query/get_estados?token=${Token}`} 
+                handleChange={(e)=>setState(e.target.value)}
+            />
             
-            {state && <SelectList title='municipios' url={``} handleChange={(e)=>setMunicipios(e.target.value)}/> }
+            
+            {state && 
+            <SelectList 
+                title='municipios' 
+                url={``} 
+                handleChange={(e)=>setMunicipios(e.target.value)}
+            /> }
 	        
             {municipios && <SelectList title='colonias' url={``} handleChange={(e)=>setColonias(e.target.value)}/>}
 	        
-            <p>{state,municipios,colonias}</p>
+            {/* <p>{state,municipios,colonias}</p> */}
             
         </>
     )
