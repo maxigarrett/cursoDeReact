7f4e6cf3d9f79d8d5e83f0e038c63b07760271e6
maxigarrett <62960428+maxigarrett@users.noreply.github.com>
2021-06-24 10:59:51 -0300

    Update SelectList.js

--- 22-select-anidados-api-paises/src/component/SelectList.js
+++ 22-select-anidados-api-paises/src/component/SelectList.js
@@ -1,16 +1,28 @@
 import React from 'react';
-// import { FetchApi } from '../helper/FetchApi';
+import { FetchApi } from '../helper/FetchApi';
+import {Message} from './Message';
 
-export const SelectList=({title,url,handleChange})=>{
-    // const {data,error,loading}= FetchApi(url);
+export const SelectList= ({title,url,handleChange})=>{
+    const {data,error,loading}= FetchApi(url);
+    if(!data){
+        return null
+    } 
+    if(error){
+        return <Message MesaggeError={`Error ${error.satusText}:${error.status}`} BgColor='#dc3545'/>
+    }
 
-    // let id=`select-${title}`
-    // let options=data.response;
+    
+    
+    let id=`select-${title}`
+    let options=data.response[title];
+    console.log(data,error,loading)
+    console.log(options)
     return(
         <>
             <h2>Select List</h2>
-            <select name={''} onChange={handleChange}>
+            <select name={id} onChange={handleChange}>
                 <option value=''>-- elige {title} --</option>
+                {data && options.map((el)=><option key={el} value={el}>{el}</option>)}
             </select>
         </>
 
