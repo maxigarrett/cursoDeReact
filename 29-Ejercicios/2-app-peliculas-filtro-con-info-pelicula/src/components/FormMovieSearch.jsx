f7bb376e6bd5c049d2bba82e07b5fd7df0f1f201
maxigarrett <maxyg22@outlook.es>
2022-06-13 12:32:24 -0300

    modificaciones y  sacado de boton de formulario

--- 29-Ejercicios/2-app-peliculas-filtro-con-info-pelicula/src/components/FormMovieSearch.jsx
+++ 29-Ejercicios/2-app-peliculas-filtro-con-info-pelicula/src/components/FormMovieSearch.jsx
@@ -1,47 +1,45 @@
-import React from 'react'
-import { useState } from 'react'
-import { useHistory } from "react-router-dom"
-import  style from './FormMovieSearch.module.css'
-const initialFormSearch={
-    search:''
-}
-export  const FormMovieSearch=({setMovies,setPageNumeration})=> {
-    const [form, setForm] = useState(initialFormSearch)
-    const history = useHistory();
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import style from "./FormMovieSearch.module.css";
+const initialFormSearch = {
+  search: "",
+};
+export const FormMovieSearch = ({ setMovies, setPageNumeration }) => {
+  const [form, setForm] = useState(initialFormSearch);
+  const history = useHistory();
 
+  const handelChange = (e) => {
+    const { name, value } = e.target;
+    setMovies([]);
+    setPageNumeration(1);
+    setForm({
+      ...form,
+      [name]: value,
+    });
+    history.push(`?search=${value}`);
+  };
 
-    const handelChange=(e)=>{
-        const {name,value}=e.target
-        setMovies([])
-        setPageNumeration(1)
-        setForm({
-            ...form,
-            [name]:value
-        })
-        history.push(`?search=${value}`)
-        
-    }
- 
-    const handelSubmit=(e)=>{
-        e.preventDefault()
-        if(!form.search || form.search==='') return alert('ingrese algun dato a buscar');
-      
-        //reseteamos las variable para que no concatene lo que hay con lo nuevo buscado
-        
-        history.push(`?search=${form.search}`);
-        setMovies([])
-        setPageNumeration(1)
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (!form.search || form.search === "")
+      return alert("ingrese algun dato a buscar");
 
-    }
-    return (
-            <form onSubmit={handelSubmit} className={style.form}>
-                <input 
-                    type="text"
-                    name='search'
-                    onChange={ handelChange}
-                    value={form.search} 
-                />
-                <button type="submit">buscar</button>
-            </form>     
-    )
-}
+    //reseteamos las variable para que no concatene lo que hay con lo nuevo buscado
+    // setMovies([]); si limpio el array no me muestra la pelicula cuando el usuario da clic en buscar o enter
+    setPageNumeration(1);
+
+    history.push(`?search=${form.search}`);
+  };
+  return (
+    <form onSubmit={handelSubmit} className={style.form}>
+      <input
+        className={style.inputSearchMovie}
+        type="text"
+        name="search"
+        onChange={handelChange}
+        value={form.search}
+      />
+    </form>
+  );
+};
--- 29-Ejercicios/2-app-peliculas-filtro-con-info-pelicula/src/pages/MovieDescription.jsx
+++ 29-Ejercicios/2-app-peliculas-filtro-con-info-pelicula/src/pages/MovieDescription.jsx
@@ -1,36 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { useEffect } from "react/cjs/react.development";
+// import { useEffect } from "react/cjs/react.development";
 import { get } from "../helper/helHttp";
-import styles from '../components/MovieList.module.css'
-import stylesMovieDescription from './MovieDescription.module.css';
-export const MovieDescription=()=>{
-    //capturamos el id de la url al hacer clic en una pelicula
-    const {idMovie}=useParams();
-    const [movieDetail,setMovieDetail]=useState(null);
-    const url=`https://api.themoviedb.org/3/movie/${idMovie}}`;
-    useEffect(()=>{
-        get(url).then(data=>{
-            console.log(data)
-            setMovieDetail(data)
-        })
-    },[url])
-    if(!movieDetail) return null
-    
-    const imgDetails=`https://image.tmdb.org/t/p/w200/${movieDetail.poster_path}`;
-    return(
-        <section className={`${stylesMovieDescription.section}`}>
-            <div>
-                {/* <span>{movieDetail.original_language}</span>  */}
-                <img src={imgDetails} className={styles.movieImg} alt='img'/>
-                <div>
-                    <p>{movieDetail.original_title}</p> 
-                </div>
-            </div>
-            <div className={`${stylesMovieDescription.description}`}>
-               <strong>Description: </strong> 
-               {movieDetail.overview}
-            </div>
-        </section>
-    )
-} 
+import styles from "../components/MovieList.module.css";
+import stylesMovieDescription from "./MovieDescription.module.css";
+export const MovieDescription = () => {
+  //capturamos el id de la url al hacer clic en una pelicula
+  const { idMovie } = useParams();
+  const [movieDetail, setMovieDetail] = useState(null);
+  const url = `https://api.themoviedb.org/3/movie/${idMovie}}`;
+  useEffect(() => {
+    get(url).then((data) => {
+      console.log(data);
+      setMovieDetail(data);
+    });
+  }, [url]);
+  if (!movieDetail) return null;
+
+  const imgDetails = `https://image.tmdb.org/t/p/w200/${movieDetail.poster_path}`;
+  return (
+    <section className={`${stylesMovieDescription.section}`}>
+      <div>
+        {/* <span>{movieDetail.original_language}</span>  */}
+        <img src={imgDetails} className={styles.movieImg} alt="img" />
+        <div>
+          <p>{movieDetail.original_title}</p>
+        </div>
+      </div>
+      <div className={`${stylesMovieDescription.description}`}>
+        <strong>Description: </strong>
+        {movieDetail.overview}
+      </div>
+    </section>
+  );
+};
