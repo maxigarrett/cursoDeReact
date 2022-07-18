08ed800ccf059e873a40c8d1bda597c2f6cb04c9
maxigarrett <maxyg22@outlook.es>
2022-07-18 11:31:19 -0300

    agregando comentario papra los dispatch del shopingcart de carpeta redux

--- 28.1- REDUX/1-redux-contador-crud-carrito/src/components/ShoppingCar.js
+++ 28.1- REDUX/1-redux-contador-crud-carrito/src/components/ShoppingCar.js
@@ -1,33 +1,43 @@
-import { useDispatch, useSelector } from "react-redux"
-import { addToCar, clearCar, deleteCar } from "../actions/shoppengCardAction"
-import { ShoppingCarItem } from "./ShoppingCarItem"
-import { ShoppingProducts } from "./ShoppingProducts"
+import { useDispatch, useSelector } from "react-redux";
+import { addToCar, clearCar, deleteCar } from "../actions/shoppengCardAction";
+import { ShoppingCarItem } from "./ShoppingCarItem";
+import { ShoppingProducts } from "./ShoppingProducts";
 
-export const ShoppingCar=()=>{
-    const state = useSelector(state => state)
-    const dispatch = useDispatch();
-    console.log(state)
-    const {products,car}= state.shoping;
+export const ShoppingCar = () => {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  console.log(state);
+  const { products, car } = state.shoping;
 
+  return (
+    <>
+      <h1>Carrito De compras useReducer</h1>
+      <section className="box">
+        {products &&
+          products.map((product) => (
+            <ShoppingProducts
+              key={product.id}
+              data={product}
+              //le tenemos que pasar el id aca y cuando se ejecute esta funcion mediante el clic ya tendra el id
+              //no es necesario pasarle al boton el id porque ya se lo pasamos aca al dispatch dentro de la funcion
+              addToCar={() => dispatch(addToCar(product.id))}
+            />
+          ))}
+      </section>
 
-    return (
-        <>
-            <h1>Carrito De compras useReducer</h1>
-            <section className='box'>
-                {products && products.map((product)=>
-                    <ShoppingProducts key={product.id} data={product} addToCar={()=>dispatch(addToCar(product.id))}/>)}
-            </section>
-
-            <section>
-                <button style={{width:'100px'}} onClick={()=> dispatch(clearCar())}>Clear Car</button>
-                {car.map((item,index)=>
-                    <ShoppingCarItem 
-                        key={index} 
-                        data={item} 
-                        delOneFromCar={()=> dispatch(deleteCar(item.id))}
-                        delAllFromCar={()=> dispatch(deleteCar(item.id,true))}
-                    />)}
-            </section>
-        </>
-    )
-}
+      <section>
+        <button style={{ width: "100px" }} onClick={() => dispatch(clearCar())}>
+          Clear Car
+        </button>
+        {car.map((item, index) => (
+          <ShoppingCarItem
+            key={index}
+            data={item}
+            delOneFromCar={() => dispatch(deleteCar(item.id))}
+            delAllFromCar={() => dispatch(deleteCar(item.id, true))}
+          />
+        ))}
+      </section>
+    </>
+  );
+};
--- 28.1- REDUX/1-redux-contador-crud-carrito/src/components/ShoppingProducts.js
+++ 28.1- REDUX/1-redux-contador-crud-carrito/src/components/ShoppingProducts.js
@@ -1,10 +1,14 @@
-export const ShoppingProducts=({data,addToCar})=>{
-    const{id,name,price}=data
-    return(
-        <article className='articleCard'>
-            <h3>{name}</h3>
-            <p>{price}</p>
-            <button onClick={()=>addToCar(id)}>Add To Car</button>
-        </article>
-    )
-}
+export const ShoppingProducts = ({ data, addToCar }) => {
+  const { id, name, price } = data;
+  return (
+    <article className="articleCard">
+      <h3>{name}</h3>
+      <p>{price}</p>
+      {/* como el id se lo pasamos ya en el dispatch seria muy redundante
+      ponerselo a la funcion addToCar(id) andaria igual pero ya le pasamos el
+      id a este componente ShoppingProducts que esta en ShoppingCar.entonces 
+      al hacer clic mandara ese id que esta como propiedad de este componente*/}
+      <button onClick={() => addToCar()}>Add To Car</button>
+    </article>
+  );
+};
